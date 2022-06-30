4198d02720828a1787f83193a40d1e25ca7e404c
AlterJuice <alterjuice@users.noreply.github.com>
2022-06-30 17:57:42 +0300

    +version

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='JTelebotUtils',
-    version='1.1',
-    packages=["."],
+    version='1.11',
+    packages=["JTelebotUtils"],
     url='github.com/AlterJuice',
     requires=["telebot"],
     license='',
