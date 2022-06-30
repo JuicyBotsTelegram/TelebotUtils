2dbedd5c02037f71eb937e887aa47863071e57ce
AlterJuice <alterjuice1758@gmail.com>
2022-06-30 18:41:49 +0300

    Fixed annotations. Added Callaback query extensions

--- JTelebotUtils/Extensions/TMsgExt.py
+++ JTelebotUtils/Extensions/TMsgExt.py
@@ -3,15 +3,18 @@ import telebot
 from JTelebotUtils.Models.ChatTypes import ChatTypes
 
 
-def isFromGroup(msg: telebot.types.Message):
+def isFromGroupOrPrivate(msg: telebot.types.Message) -> bool:
+    return isFromPrivate(msg) or isFromGroup(msg)
+
+
+def isFromGroup(msg: telebot.types.Message) -> bool:
     return msg.chat.type in [ChatTypes.SUPERGROUP, ChatTypes.GROUP]
 
 
-def isFromPrivate(msg: telebot.types.Message):
+def isFromPrivate(msg: telebot.types.Message) -> bool:
     return msg.chat.type == ChatTypes.PRIVATE
 
 
-def isFromChannel(msg: telebot.types.Message):
+def isFromChannel(msg: telebot.types.Message) -> bool:
     return msg.chat.type == ChatTypes.CHANNEL
-
 
