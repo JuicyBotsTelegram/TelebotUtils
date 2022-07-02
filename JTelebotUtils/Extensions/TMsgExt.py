e0b4a8cfc4af8282904fbf31e021c802949c41d3
AlterJuice <alterjuice1758@gmail.com>
2022-07-02 14:59:13 +0300

    Created getCommandFromMsg (the old name is true_command)

--- JTelebotUtils/Extensions/TMsgExt.py
+++ JTelebotUtils/Extensions/TMsgExt.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+from typing import Optional
+
 import telebot
 # TMsgExt means TelebotMessageExtensions
 from JTelebotUtils.Models.ChatTypes import ChatTypes
@@ -23,3 +27,13 @@ def isMsgTextStartsWith(msg: telebot.types.Message, template) -> bool:
     if msg.text:
         return msg.text.startswith(template)
     return False
+
+
+def getCommandFromMsg(msg: telebot.types.Message) -> str:
+    def unpack(text: str, length: Optional[int]) -> str:
+        if '@' not in text[:length]:
+            return text[:length]
+        return text[:length].split('@')[0]
+
+    command_length = msg.entities[0].length if '/' == msg.text[0] else None
+    return unpack(msg.text, command_length)
